126a940f776566f2b49885b6d974821209430a1d
Mihai Gisca <mihai.gisca1996@gmail.com>
2025-07-06 12:34:25 +0300

    add assignments file with values and variables exercise

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,6 @@
+let country = "Republic of Moldova";
+let continent = "Europe";
+let population = 2.458;
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population (in millions): " + population);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,13 @@
 let js = "amazing";
-if (js === "amazing") alert("JS is fun!");
-
 console.log(30 + 20 + 15 + 5 - 1);
+
+console.log("Mihai");
+console.log(29);
+
+// variable - box that stores value
+// javascript convention - camel case: first word lower case, next words upper case
+// there are words reserved by javascript that cannot be used as variable name (e.g., new, function)
+// name of variable can only contain letters, digits, underscore and dollar sign
+// name of variable cannot start with digit
+let firstName = "Mihai";
+console.log(firstName);
