3a518facbe71b0c2aa0b1d4d6f035b0bc255323a
Mihai Gisca <mihai.gisca1996@gmail.com>
2025-07-06 13:48:30 +0300

    add data types lecture and exercise

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,6 +1,18 @@
+// Lecture: Values and Variables
 let country = "Republic of Moldova";
 let continent = "Europe";
 let population = 2.458;
-console.log("Country: " + country);
-console.log("Continent: " + continent);
-console.log("Population (in millions): " + population);
+// console.log("Country: " + country);
+// console.log("Continent: " + continent);
+// console.log("Population (in millions): " + population);
+
+// Lecture: Data Types
+let isIsland = false;
+let language;
+console.log("Country: " + country + " (" + typeof country + ")");
+console.log("Continent: " + continent + " (" + typeof continent + ")");
+console.log(
+  "Population (in millions): " + population + " (" + typeof population + ")"
+);
+console.log("Is island: " + isIsland + " (" + typeof isIsland + ")");
+console.log("Language: " + language + " (" + typeof language + ")");
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,5 @@
+/* 
+// Lecture: Values and Variables
 let js = "amazing";
 console.log(30 + 20 + 15 + 5 - 1);
 
@@ -11,3 +13,51 @@ console.log(29);
 // name of variable cannot start with digit
 let firstName = "Mihai";
 console.log(firstName);
+ */
+
+// Lecture: Data Types
+let piName = "PI";
+let piValue = 3.14;
+let isPi = true;
+if (isPi) {
+  console.log(
+    piName +
+      " (" +
+      typeof piName +
+      ")" +
+      ": " +
+      piValue +
+      " (" +
+      typeof piValue +
+      ")"
+  );
+}
+
+// Dynamic typing example:
+// changing the value type during variable reassignment
+// initially was number, now it is string
+piValue = "3.14";
+if (isPi) {
+  console.log(
+    piName +
+      " (" +
+      typeof piName +
+      ")" +
+      ": " +
+      piValue +
+      " (" +
+      typeof piValue +
+      ")"
+  );
+}
+
+// Undefined variable
+let year;
+console.log(year + " (" + typeof year + ")");
+// Define variable
+year = 2025;
+console.log(year + " (" + typeof year + ")");
+
+// Bug: typeof null returns object, which has no sense,
+// the bug is maintained for legacy reasons
+console.log(null + " (" + typeof null + ")");
