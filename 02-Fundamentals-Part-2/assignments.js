8d8f01821bb6cce2188f6ed113d5f4905f283315
MihaiGisca <mihai.gisca@mechatronics-center.com>
2025-07-15 07:38:49 +0300

    add function declarations vs expressions lecture and assignment

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -1,3 +1,5 @@
+// Fundamentals Part 2
+/* 
 // Lecture: Functions
 function describeCountry(country, populationInMillions, capitalCity) {
     return `${country} has ${populationInMillions} people and its capital city is ${capitalCity}.`;
@@ -11,4 +13,26 @@ console.log(romaniaDescription);
 
 const usaDescription = describeCountry("USA", 340.1, "Washington DC");
 console.log(usaDescription);
+ */
 
+// Lecture: Function Declarations vs. Expressions
+function percentageOfWorld1(populationInMillions) {
+    return populationInMillions * 100.0 / 7900;
+}
+
+console.log(`China population is about ${percentageOfWorld1(1411)}% of the world population.`);
+console.log(`Republic of Moldova population is about ${percentageOfWorld1(2.458)}% of the world population.`);
+console.log(`Brazil population is about ${percentageOfWorld1(211.1)}% of the world population.`);
+
+const percentageOfWorld2 = function(populationInMillions) {
+    return populationInMillions * 100.0 / 7900;
+}
+
+console.log(`India population is about ${percentageOfWorld2(1438)}% of the world population.`);
+console.log(`Japan population is about ${percentageOfWorld2(124.5)}% of the world population.`);
+console.log(`Europe population is about ${percentageOfWorld2(742.3)}% of the world population.`);
+console.log(`Africa population is about ${percentageOfWorld2(1373)}% of the world population.`);
+console.log(`Asia population is about ${percentageOfWorld2(4694.6)}% of the world population.`);
+console.log(`North America population is about ${percentageOfWorld2(528.7)}% of the world population.`);
+console.log(`South America population is about ${percentageOfWorld2(442)}% of the world population.`);
+console.log(`Australia population is about ${percentageOfWorld2(26.6)}% of the world population.`);
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -14,6 +14,7 @@ if (hasDriversLicense) console.log("I can drive ^_^");
 // const interface = "Audio";
 // const private = 534;
 
+/* 
 // Lecture: Functions
 // function function_name() {
 //   function body
@@ -22,7 +23,7 @@ function logger() {
     console.log("My name is Mike")
 }
 
-// invoking / calling / running function
+// invoking / calling / running / executing function
 logger();
 logger();
 logger();
@@ -47,3 +48,27 @@ const appleOrangeJuice = fruitProcessor(2,2);
 console.log(appleOrangeJuice);
 
 // DRY principle - Don't Repeat Yourself (i.e., using functions is one way to achieve that)
+ */
+
+// Lecture: Function Declarations vs. Expressions
+// Function declaration
+// function CAN be called before it is declared
+const age1 = calcAge1(1998);
+function calcAge1(birthYear) {
+    return 2069 - birthYear;
+}
+
+// Function expression
+// function CANNOT be called before it is declared
+// This will not work:
+// const age2 = calcAge2(1998);
+
+// stored into a variable (thus is a value)
+// called the same
+// anonymous(?)
+const calcAge2 = function (birthYear) {
+    return 2069 - birthYear;
+}
+const age2 = calcAge2(1998);
+
+console.log(age1, age2);
