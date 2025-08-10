cd706af05684ae1f82ab06c64963c81882fcfd5d
Mihai Gisca <mihai.gisca1996@gmail.com>
2025-08-10 17:13:21 +0300

    add Working With Classes lecture

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -12,5 +12,37 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
+const showModal = function () {
+  // JS allows adding/removing classes dinamically to elements
+  // here we don't use the . because we are just passing the class name
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// for (let i = 0; i < btnsShowModal.length; i++)
+//   btnsShowModal[i].addEventListener('click', function () {
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
+
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
 for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+  // JS allows to pass a function as an argument which improves the code readability
+  btnsShowModal[i].addEventListener('click', showModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
