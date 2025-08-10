f429d12f066f23589804dbe8391ac44d83059ac3
Mihai Gisca <mihai.gisca1996@gmail.com>
2025-08-10 12:29:43 +0300

    add what is DOM and DOM manipulation lecture

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -3,3 +3,18 @@
 // Lecture: PROJECT #1: Guess My Number!
 // console.log(document.querySelector('.message'));
 console.log(document.querySelector('.message').textContent);
+
+// Lecture: What's the DOM and DOM Manipulation
+// Document Object Model (DOM) - structured representation of HTML documents that allows JS to access and manipulate HTML elements and styles.
+// DOM - complete respresentation of HTML document.
+// DOM has a tree structure called DOM tree.
+// Document - <html> - <head> - <title> - text
+//         |- <body> - <section> - <p> - text
+//                  |           |- <p> - text
+//                  |           |- <a>
+//                  |- <section> - <img>
+//
+// DOM is not JS
+// - JS is implementation of ECMA rules
+// - DOM and DOM methods are part of web APIs
+// - DOM and JS can interact
