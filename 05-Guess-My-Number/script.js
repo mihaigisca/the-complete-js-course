b65fe2fdec016344e846fb55431d12ceea279d59
Mihai Gisca <mihai.gisca1996@gmail.com>
2025-08-10 15:04:37 +0300

    add challenge #1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -45,7 +45,11 @@ console.log(document.querySelector('.guess').value);
 //   > eventHandler function
 
 // Lecture:  Implementing the Game Logic
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+const generateSecretNumber = function () {
+  return Math.trunc(Math.random() * 20 + 1);
+};
+
+let secretNumber = generateSecretNumber();
 
 // data is part of application state when it is relevant for proper app functioning
 // such data has to be in the code and not in the DOM
@@ -95,4 +99,32 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🤣 You Lost the Game!';
     }
   }
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+
+  document.querySelector('.number').textContent = '?';
+  secretNumber = generateSecretNumber();
+
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('.guess').value = '';
 });
