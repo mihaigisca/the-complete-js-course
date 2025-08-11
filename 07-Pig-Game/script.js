7bfc3de0aa282f738c959037938bb650399fa740
Mihai Gisca <mihai.gisca1996@gmail.com>
2025-08-11 23:19:32 +0300

    add PROJECT #3: Pig Game initial lecture

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Lecture: PROJECT #3: Pig Game
+// application flow chart - representation of anything that can happen in an application
+// start with a raw sketch to get the big picture of the application flow
+// selecting an element by class: document.querySelector('.class-name')
+// selecting an element by ID, method 1: document.querySelector('#id-name') // note # is present
+// selecting an element by ID, method 2: document.getElementById('id-name') // note # is missing
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+// diceEl.style.display = 'none';
+diceEl.classList.add('hidden');
