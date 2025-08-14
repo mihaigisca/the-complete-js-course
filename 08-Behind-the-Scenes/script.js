ea0ada5543684c7bdcec154f6f4b4361ab38b981
Mihai Gisca <mihai.gisca1996@gmail.com>
2025-08-14 19:14:06 +0300

    add An High-Level Overview of JavaScript lecture

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Lecture: An High-Level Overview of JavaScript
+// The tip of the iceberg: JS is a high-level, object-oriented, multi-paradigm programming language
+// JS is a high-level, prototype-based object-oriented, multi-paradigm, interpreted or just-in-time compiled, dynamic, single-threaded, garbage-collected programming language with first class functions and a non-blocking event loop concurrency model
+
+// high-level - developer does NOT have to worry about resources, everything happens automatically
+// garbage-collected - cleaning old objects from the computer memory
+// interpreted or just-in-time compiled language - abstraction over 0s and 1s (machine code) happens in JS Engine
+// multi-paradigm - approaches and mindsets of structuring code, which will direct coding styles and techniques
+// - procedural programming
+// - object-oriented programming (OOP)
+// - functional programming (FP)
+// prototype-based object-oriented - everything are objects, expect primitive types
+// first-class functions - functions are treated as regular variables
+// dynamic - types become known at runtime and are automatically changed
+// single-threaded - can only do one thing at a time
+// - concurrency model: how JS engine handles multiple tasks happening at the same time
+// non-blocking event loop - takes long-running tasks, executes them in "background", and puts them back in the main thread once they are finished
