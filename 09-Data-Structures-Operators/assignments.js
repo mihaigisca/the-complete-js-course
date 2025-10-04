9793396c08c74d391801419611f5de8cbea3bf9e
Mihai Gîsca <mihai.gisca1996@gmail.com>
2025-10-04 21:28:00 +0300

    add Enhanced Object Literals assignment

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -379,3 +379,26 @@ const books = [
 // for (const [i, el] of allAuthors.entries()) {
 //   console.log(`${i + 1}: ${el}`);
 // }
+
+// -------------------------------------------------------------------------------------------------------------
+// Lecutre: Enhanced Object Literals
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: [...bookData[1][1]],
+  [bookData[2][0]]: [bookData[2][1]],
+};
+console.log(newBook);
+
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+};
